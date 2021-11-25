c63e46796d18809ae7b364694c459da57d2aeb19
raluvy95 <ralucadoge069@gmail.com>
2021-11-25 20:18:32 +0200

    Added bookmarks support!!!

--- src/options.js
+++ src/options.js
@@ -1,11 +1,12 @@
 function load() {
     const config = window.localStorage
     const enable = JSON.parse(config.enableQuickstart)
-    if(enable) {
+    if (enable) {
         document.getElementById("enableQuickstart").setAttribute("checked", '')
     } else return;
     quickstartList()
 }
+
 function newQuickstart() {
     saveQuickstart()
     const nou = {
@@ -17,8 +18,31 @@ function newQuickstart() {
     window.localStorage.setItem("quickstart", JSON.stringify(json))
     quickstartList()
 }
+
+function loadFromBookmarks() {
+    chrome.bookmarks.getTree().then(r => {
+        if (r.length < 1 || r[0].children.length < 1 ||
+            r[0].children[0].children.length < 1) {
+            return alert("Looks like you have empty bookmarks.")
+        }
+        const holup = confirm("Do you want to load from bookmarks? Your current quick shortcut will be overwritten.")
+        if (!holup) return;
+        const element = r[0].children[0].children.filter(m => !m.children)
+        let result = []
+        for (const obj of element) {
+            result.push({
+                "title": obj.title,
+                "url": obj.url
+            })
+            window.localStorage.setItem("quickstart", JSON.stringify(result))
+        }
+        quickstartList()
+        document.getElementById("saveConfig").setAttribute("value", "Saved!")
+        setTimeout(() => document.getElementById("saveConfig").setAttribute("value", "Save"), 2000)
+    })
+}
+
 function removeQuickstart(e) {
-    console.log("IT WORKS")
     saveQuickstart()
     const ID = e.target.id.slice(16)
     const json = JSON.parse(window.localStorage.quickstart)
@@ -26,9 +50,12 @@ function removeQuickstart(e) {
     window.localStorage.setItem("quickstart", JSON.stringify(json))
     quickstartList()
 }
+
 function quickstartList() {
     document.getElementById("quickstart-list").innerHTML = ''
-    let general = '<button id="newquick">New</button><ul style="display: flex; justify-content: center; flex-direction: column; list-style-type: none;">'
+    let general = `<button id="newquick">New</button>
+    <button id="loadbookmarks">Load from Bookmarks</button>
+    <ul style="display: flex; justify-content: center; flex-direction: column; list-style-type: none;">`
     const quickstart = JSON.parse(window.localStorage.quickstart)
     if (quickstart.length < 1) general += "<p>Empty quickstart</p>"
     else {
@@ -37,31 +64,31 @@ function quickstartList() {
             general += `
                 <li style="margin: 10px;">
                 <label for="quickstartTitle${count}">Title</form>
-                <input type="text" id="quickstartTitle${count}" value=${obj.title}>
+                <input type="text" id="quickstartTitle${count}" value="${obj.title}">
                 <label for="quickstartUrl${count}">URL</form>
-                <input type="text" id="quickstartUrl${count}" value=${obj.url}>
+                <input type="text" id="quickstartUrl${count}" value="${obj.url}">
                 <input type="button" id="quickstartRemove${count}" value="Remove">
                 </li>
             `
             count++
         }
         general += "</ul>"
-        setTimeout(() => { 
+        setTimeout(() => {
             // I added 1s delay due to not loading stuff before this.
             const removeButtons = !document.querySelectorAll("[id^='quickstartRemove']") ? [] : Array.from(document.querySelectorAll('[id^="quickstartRemove"]'))
-            console.log(removeButtons)
-            for(const button of removeButtons) {
+            for (const button of removeButtons) {
                 button.addEventListener("click", removeQuickstart)
             }
         }, 1000)
     }
     document.getElementById("quickstart-list").innerHTML = general
     document.getElementById("newquick").addEventListener("click", newQuickstart)
+    document.getElementById("loadbookmarks").addEventListener("click", loadFromBookmarks)
 }
 
 document.getElementById("enableQuickstart").addEventListener("change", () => {
     const enable = JSON.parse(window.localStorage.enableQuickstart)
-    if(!enable) {
+    if (!enable) {
         window.localStorage.setItem("enableQuickstart", "true")
         quickstartList()
     } else {
@@ -73,9 +100,9 @@ function saveQuickstart() {
     let count = 0;
     let result = []
     const stop = setInterval(() => {
-        const title = document.getElementById("quickstartTitle"+count)
-        const url = document.getElementById("quickstartUrl"+count)
-        if(!title && !url) clearInterval(stop);
+        const title = document.getElementById("quickstartTitle" + count)
+        const url = document.getElementById("quickstartUrl" + count)
+        if (!title && !url) clearInterval(stop);
         else {
             result.push({
                 "title": title.value,
@@ -86,6 +113,7 @@ function saveQuickstart() {
         count++
     }, 20)
 }
+
 document.getElementById("saveConfig").addEventListener("click", () => {
     saveQuickstart()
     document.getElementById("saveConfig").setAttribute("value", "Saved!")
